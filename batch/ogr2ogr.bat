216348ff1104292329eded6383f667eab8c9118b
Carlos Padron <padron.ca@gmail.com>
2019-02-27 15:48:12 +0000

    Update ogr2ogr.bat

--- batch/ogr2ogr.bat
+++ batch/ogr2ogr.bat
@@ -1,4 +1,4 @@
 rem appends gz files to postgis
-for %%A in ("I:\somwhere\*.gz") do ^
+for %%A in ("%~dp0\somwhere\*.gz") do ^
 ogr2ogr -append -a_srs EPSG:27700 -f ^ 
-"PostgreSQL" PG:"host=localhost schemas=schema user=postgres dbname=gis password=pass" "%%~fA"
+"PostgreSQL" PG:"host=localhost schemas=schema user=postgres dbname=gis password=pass" "%%~fA" -lco LAUNDER=NO
